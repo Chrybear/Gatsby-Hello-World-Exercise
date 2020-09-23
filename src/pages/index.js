f797b95752f6919c37c9bc4cec2e3603f7b8b48e
unknown <charles_barrett24@mymail.eku.edu>
2020-09-22 20:18:15 -0400

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import DK from "../../static/ooh, banana.png"
+
+export default function Home() {
+    return <div><img src ={DK} alt="Oh, Banana"/><br/>Oh, banana</div>
+}
